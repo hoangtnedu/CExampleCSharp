a0b5d9f1ef9be3d80094f18e52146a8da9efb0a6
HoangTN <HoangTN@DESKTOP-H68FGFI>
2022-03-12 16:54:55 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Chao mung cac ban VTC den voi Lap trinh .NET");
+            Console.Write("\n Nhập n= ");
+            int n;
+            n=int.Parse(Console.ReadLine());
+            Console.Write("Số vừa nhập là {0}",n);
+        }
+    }
+
+
+
+}
